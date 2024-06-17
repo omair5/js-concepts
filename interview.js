3021b32fcdce04dd6f9a1f59b6981a91d152058e
omair5 <muhammad.omair235@gmail.com>
2024-06-18 04:51:34 +0500

    class in javascript

---
+++ interview.js
@@ -0,0 +1,95 @@
+//-------- JAVASCRIPT
+// call stack => execution context / memory allocation
+// heap memory
+// hoisting
+// closures
+// constructor functions vs classes
+// this => call/apply/bind
+// memory management
+// prmosies
+// promises methods
+// observables
+// event delegation and bubbling
+// event loop
+// currying
+// pure functions
+// generator functions
+// recursion
+// getter setter
+// singleton pattern
+// OOP
+// prototype
+// prototypal inheritance
+// browser Api
+// DOM
+// object methods like : freeze / seal
+// Array methods like : splice / slice
+// mutability vs immutability
+// primitive vs non primitive
+// type coercion
+// callback
+// A callback is a JavaScript function that is passed to another function as an argument or a parameter.This function is to be executed whenever the function that it is passed to gets executed.
+
+// sessionStorage vs localStorage vs indexedDB
+// spread vs concat
+//  difference between let and var
+// arrow functions vs normal functions
+// == vs ===
+// deep vs shallow copy
+// async vs defer
+
+
+//-------- REACT
+// what is SPA?
+// react under the hood
+// JWT HANDLING
+// useRef / createRef / forwardRef
+// useImperative
+// useTransition
+// usEId
+// strictmode
+// HOC
+// rerendering in react
+// concept of key
+// folder structure
+// Dynamic Imports
+// useMemo and callBack
+// virtual dom
+// diffing
+// reconcilation
+// how to make perfomance better of a react app
+
+
+//-------- LOGICAL JS QUESTIONS
+// 1- give an array [[[[]]]]] check whether all have opening and closing bracket partners
+// flaten arrays using for loog
+// find common characters in input
+// 111223(312231)
+// second largest number
+
+
+
+//-------- LOGICAL REACT QUESTIONS
+// https://www.youtube.com/watch?v=DCoIeGt4g7M
+// https://www.youtube.com/watch?v=6euTBVDCD6o&list=PL4ruoTJ8LTT8250F2ZrYVmRO6o5gWZKpG
+// https://www.youtube.com/watch?v=7DJCpgc6V94&list=PL4ruoTJ8LTT8250F2ZrYVmRO6o5gWZKpG
+// https://www.youtube.com/watch?v=Iv1gZN900uc&list=PL4ruoTJ8LTT8250F2ZrYVmRO6o5gWZKpG
+// https://www.youtube.com/watch?v=3OxMsnP78zg
+// https://www.youtube.com/watch?v=-uAhf92DVqA
+// https://www.youtube.com/watch?v=yHg0bvNqJEQ
+
+
+
+//-------- CSS
+// box model
+// semantics tags
+// div vs span
+// display:none vs visibility:hidden
+
+
+
+//-------- THING TO SEARCH
+
+// does useCallback have the whole function defintion ??
+// threading in javascript
+// concurrent vs parallel
--- script.js
+++ script.js
@@ -499,3 +499,409 @@
 
 // return this;  (implicitly)
 // }
+
+
+
+
+// ---------------------- CLASS IN JAVASCRIPT
+
+// class = (ES6 feature) provides a more structured and cleaner way to
+// work with objects compared to traditional constructor functions
+
+// In JavaScript, the constructor inside a class and a constructor function serve similar purposes but have different syntaxes and are used in different contexts
+
+// class product {
+//     constructor(name, price) {
+//         this.name = name
+//         this.price = price
+//     }
+
+//     displayProducts() {
+//         console.log(`product : ${this.name}`)
+//         console.log(`price : ${this.price}`)
+//     }
+//     calculateTotal(salesTax) {
+//         return this.price + (this.price * salesTax);
+//     }
+// }
+
+// we just need to call the class, the constructor function will be called automatically
+
+// const product1 = new product()
+// console.log('product1', product1)
+
+// in above console we are only getting
+// { name: undefined, price: undefined }
+// there is no method available in the object
+
+// In JavaScript, when you define a class method, it is actually stored on the prototype
+// of the class rather than directly on each instance of the class.
+// This approach helps save memory because all instances of the class share the same method
+// definition, rather than each instance having its own copy of the method.
+
+// Why Methods Aren't Directly on the Object
+// When you log product1, you see only the properties(name and price).
+// The displayProducts method isn't shown directly on product1 because it's on Product.prototype.However,
+// you can still call product1.displayProducts() because JavaScript will look up the prototype chain and find the method there.
+
+// Visualizing the Prototype Chain
+
+// product1(instance)
+//     | __ name: "Sample Product"
+//         | __ price: 100
+//             | __ __proto__ -> Product.prototype
+//                 | __ displayProducts
+//                     | __ constructor
+
+// When you access product1.displayProducts,
+// JavaScript finds displayProducts on Product.prototype through the prototype chain.
+
+
+// product1.displayProducts()
+
+
+// Why Are Methods Defined Outside the Constructor Function ?
+
+// Memory Efficiency:
+// If methods were defined inside the constructor, every instance of the class would
+// get its own copy of each method.By defining methods on
+// the prototype(which happens when you define methods outside the constructor),
+// all instances share the same method definitions, saving memory.
+
+// Prototype Chain:
+// Defining methods outside the constructor puts them on the class's prototype.
+// This is a key aspect of how inheritance works in JavaScript.
+// When a method is called on an instance,
+// JavaScript looks up the prototype chain to find the method,
+// promoting reuse and reducing redundancy.
+
+
+// STATIC KEYWORD
+// ---------------
+
+// static = keyword that defines properties or methods that belong
+// to a class itself rather than the objects created
+// from that class (class owns anything static, not the objects)
+
+
+// ----------- EXAMPLE 1 -----------
+// class MathUtil {
+//     static PI = 3.14159;
+
+//     static getDiameter(radius) {
+//         return radius * 2;
+//     }
+//     static getCircumference(radius) {
+//         return 2 * this.PI * radius;
+//     }
+//     static getArea(radius) {
+//         return this.PI * radius * radius;
+//     }
+// }
+
+// console.log(MathUtil.PI);
+// console.log(MathUtil.getDiameter(10));
+// console.log(MathUtil.getCircumference(10));
+// console.log(MathUtil.getArea(10));
+
+
+
+
+// ----------- EXAMPLE 2 -----------
+
+// class User {
+
+//     static userCount = 0;
+
+//     constructor(username) {
+//         this.username = username;
+//         User.userCount++;
+//     }
+
+//     static getUserCount() {
+//         console.log(`There are ${User.userCount} users online`);
+//     }
+//     sayHello() {
+//         console.log(`Hello, my username is ${this.username}`);
+//     }
+// }
+
+// const user1 = new User("Spongebob");
+// const user2 = new User("Patrick");
+// const user3 = new User("Sandy");
+
+// user1.sayHello();
+// user2.sayHello();
+// user3.sayHello();
+// User.getUserCount();
+
+// type of class is a function and type of function is object so basically it is also object
+// which gets stored in heap memory
+
+
+
+// Why Use a Constructor ?
+// The constructor allows you to initialize an object with specific values passed as arguments.
+// This is useful for setting up the initial state of an object when it's created.
+
+// you might skip the constructor.This is usually the case for very simple classes or when all properties have default values that don't need customization.
+
+
+// class Animal {
+//     alive = true;
+
+//     eat() {
+//         console.log(`This ${this.name} is eating`);
+//     }
+
+//     sleep() {
+//         console.log(`This ${this.name} is sleeping`);
+//     }
+// }
+
+// const dog = new Animal();
+// dog.name = "Dog";
+// const cat = new Animal();
+// cat.name = "Cat";
+
+// dog.eat(); // This Dog is eating
+// cat.sleep(); // This Cat is sleeping
+
+
+
+// INHERITANCE
+
+// class Animal {
+
+//     alive = true;
+
+//     eat() {
+//         console.log(`This ${this.name} is eating`);
+//     }
+//     sleep() {
+//         console.log(`This ${this.name} is sleeping`);
+//     }
+// }
+
+// class Rabbit extends Animal {
+
+//     name = "rabbit";
+
+//     run() {
+//         console.log(`This ${this.name} is running`);
+//     }
+// }
+
+// class Fish extends Animal {
+
+//     name = "fish";
+
+//     swim() {
+//         console.log(`This ${this.name} is swimming`);
+//     }
+// }
+
+// class Hawk extends Animal {
+
+//     name = "hawk";
+
+//     fly() {
+//         console.log(`This ${this.name} is flying`);
+//     }
+// }
+
+// const rabbit = new Rabbit();
+// const fish = new Fish();
+// const hawk = new Hawk();
+
+// console.log(rabbit);
+// rabbit.eat();
+// rabbit.sleep();
+// rabbit.run();
+
+
+
+// Encapsulation: Focuses on restricting access to an object's internal state and
+// providing a controlled way to access and modify it. (like getter and setters)
+
+// Abstraction: Focuses on hiding the complexity of an implementation and exposing
+// only the essential features.
+
+
+
+// SUPER
+
+// super = keyword is used in classes to call the constructor or
+// access the properties and methods of a parent (superclass)
+// this = this object
+// super = the parent
+
+// class Animal {
+
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     move(speed) {
+//         console.log(`The ${this.name} moves at a speed of ${speed}mph`);
+//     }
+// }
+
+// class Rabbit extends Animal {
+
+//     constructor(name, age, runSpeed) {
+//         super(name, age);
+//         this.runSpeed = runSpeed;
+//     }
+
+//     run() {
+//         console.log(`This ${this.name} can run`);
+//         super.move(this.runSpeed);
+//     }
+// }
+
+// class Fish extends Animal {
+
+//     constructor(name, age, swimSpeed) {
+//         super(name, age);
+//         this.swimSpeed = swimSpeed;
+//     }
+
+//     swim() {
+//         console.log(`This ${this.name} can swim`);
+//         super.move(this.swimSpeed);
+//     }
+// }
+
+// class Hawk extends Animal {
+
+//     constructor(name, age, flySpeed) {
+//         super(name, age);
+//         this.flySpeed = flySpeed;
+//     }
+
+//     fly() {
+//         console.log(`This ${this.name} can fly`);
+//         super.move(this.flySpeed);
+//     }
+// }
+
+// const rabbit = new Rabbit("rabbit", 1, 25);
+// const fish = new Fish("fish", 2, 12);
+// const hawk = new Hawk("hawk", 3, 50);
+
+// rabbit.run();
+// fish.swim();
+// hawk.fly();
+
+
+
+// GETTER SETTERS
+
+// ---------- EXAMPLE 1 ----------
+// class Rectangle {
+
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     set width(newWidth) {
+//         if (newWidth > 0) {
+//             this._width = newWidth;
+//         }
+//         else {
+//             console.error("Width must be a positive number");
+//         }
+//     }
+
+//     set height(newHeight) {
+//         if (newHeight > 0) {
+//             this._height = newHeight;
+//         }
+//         else {
+//             console.error("Height must be a positive number");
+//         }
+//     }
+
+
+//     get width() {
+//         return `${this._width.toFixed(1)}cm`;
+//     }
+
+//     get height() {
+//         return `${this._height.toFixed(1)}cm`;
+//     }
+
+
+//     get area() {
+//         return `${(this._width * this._height).toFixed(1)}cm`;
+//     }
+// }
+
+// const rectangle = new Rectangle(2, 3);
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.area);
+
+// // ---------- EXAMPLE 2 ----------
+// class Person {
+
+//     constructor(firstName, lastName, age) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//     }
+
+//     set firstName(newFirstName) {
+//         if (typeof newFirstName === "string" && newFirstName.length > 0) {
+//             this._firstName = newFirstName;
+//         }
+//         else {
+//             console.error("First name must be a non-empty string");
+//         }
+//     }
+
+//     set lastName(newLastName) {
+//         if (typeof newLastName === "string" && newLastName.length > 0) {
+//             this._lastName = newLastName;
+//         }
+//         else {
+//             console.error("Last name must be a non-empty string");
+//         }
+//     }
+
+//     set age(newAge) {
+//         if (typeof newAge === "number" && newAge >= 0) {
+//             this._age = newAge;
+//         }
+//         else {
+//             console.error("Age must be a non-negative number");
+//         }
+//     }
+
+//     get firstName() {
+//         return this._firstName;
+//     }
+
+//     get lastName() {
+//         return this._lastName;
+//     }
+
+//     get fullName() {
+//         return this._firstName + " " + this._lastName;
+//     }
+
+//     get age() {
+//         return this._age;
+//     }
+// }
+
+// const person = new Person("Spongebob", "Squarepants", 30);
+
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.fullName);
+// console.log(person.age);
